a6b74cbde72cb526c6d74f3172ef3bfa594a4984
Akashbhasme0990 <akashbhasme011@gmail.com>
2023-12-28 20:33:12 +0530

    Execption handelling for user API

---
+++ src/main/java/skye/sky/Rest/userNotFound.java
@@ -0,0 +1,15 @@
+package skye.sky.Rest;
+
+public class userNotFound extends RuntimeException {
+    public userNotFound(String message) {
+        super(message);
+    }
+
+    public userNotFound(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public userNotFound(Throwable cause) {
+        super(cause);
+    }
+}
--- src/main/java/skye/sky/Rest/userRestConrtoller.java
+++ src/main/java/skye/sky/Rest/userRestConrtoller.java
@@ -2,9 +2,12 @@ package skye.sky.Rest;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import skye.sky.Entity.User;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,7 +28,12 @@ public class userRestConrtoller {
         return theUsers;
     }
     @GetMapping("/Users/{userId}")
-    public  User getUser(@PathVariable int userId){
+    public  User getUser(@PathVariable int userId)  {
+
+        if((userId >= theUsers.size())|| (userId<0)){
+            throw new userNotFound("User id no found- "+ userId);
+        }
         return  theUsers.get(userId);
     }
-}
+
+    }
